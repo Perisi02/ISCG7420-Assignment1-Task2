2745235aaa6aba11c6055e066dcd244641b723c3
Leati Perise <Perisi02@myunitec.ac.nz>
2026-05-02 17:52:32 +1200

    Add login and logout functionality

--- appointment_system/settings.py
+++ appointment_system/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = "appointment:home"
+LOGOUT_REDIRECT_URL = "appointment:home"
+LOGIN_URL = "login"
