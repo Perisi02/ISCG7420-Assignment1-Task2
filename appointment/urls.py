ba030fa1304fbffaed34cc24a8efe38f360e1936
Leati Perise <Perisi02@myunitec.ac.nz>
2026-05-02 16:55:08 +1200

    Add available appointment slots page

--- appointment/urls.py
+++ appointment/urls.py
@@ -6,4 +6,5 @@ app_name = "appointment"
 urlpatterns = [
     path("", views.home, name="home"),
     path("doctors/", views.doctor_list, name="doctor_list"),
+    path("slots/", views.slot_list, name="slot_list"),
 ]
--- appointment/views.py
+++ appointment/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from appointment.models import Doctor
+from appointment.models import Doctor, AppointmentSlot
 
 
 # Create your views here.
@@ -10,3 +10,7 @@ def home(request):
 def doctor_list(request):
     doctors = Doctor.objects.filter(is_active=True).order_by('name')
     return render(request, 'appointment/doctor_list.html', {'doctors': doctors})
+
+def slot_list(request):
+    slots = AppointmentSlot.objects.filter(is_available=True).select_related('doctor').order_by('date', 'start_time')
+    return render(request, 'appointment/slot_list.html', {'slots': slots})
